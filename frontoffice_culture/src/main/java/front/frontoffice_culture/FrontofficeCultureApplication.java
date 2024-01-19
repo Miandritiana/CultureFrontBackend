8416f81a4ad2912014d2f3fc6682b0c203beced1
Miandritiana <miandrititana21@gmail.com>
2024-01-19 15:42:15 +0300

    add folder spring boot

---
+++ frontoffice_culture/src/main/java/front/frontoffice_culture/FrontofficeCultureApplication.java
@@ -0,0 +1,13 @@
+package front.frontoffice_culture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontofficeCultureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontofficeCultureApplication.class, args);
+	}
+
+}
---
+++ frontoffice_culture/src/test/java/front/frontoffice_culture/FrontofficeCultureApplicationTests.java
@@ -0,0 +1,13 @@
+package front.frontoffice_culture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontofficeCultureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
